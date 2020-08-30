9f502413a94e6f4f9eb91c7f8882e03235bb1a1a
iisams <69070671+iisams@users.noreply.github.com>
2020-08-30 15:16:31 +0800

    Update jdtqz.js

--- liwo/jdtqz.js
+++ liwo/jdtqz.js
@@ -21,6 +21,7 @@ var taskid = []
 var taskname = []
 var message=""
 var taskmsg = ""
+const option = {"open-url":"jdmobile://virtual?params=%7B%22category%22:%22jump%22,%22des%22:%22m%22,%22url%22:%22https%3A%2F%2Fbtfront.jd.com%2Frelease%2Fgrowth%2Findex.html%23%2Fhome%22%7D"}
 
 var taskparams = {
   url:"https://ms.jr.jd.com/gw/generic/bt/h5/m/taskStatistics?_="+nowtime+"&reqData=%7B%22req%22:%7B%22pageSize%22:50,%22channelId%22:3%7D%7D",
@@ -158,8 +159,8 @@ function Sign() {
 
 
 function show(){
-    let title = "京东特权活力值签到"
-    sams.msg(title,message,taskmsg)
+    let title = "京东特权活力值签到 按时点击通知收取哦"
+    sams.msg(title,message,taskmsg,option)
 }
 
 async function dotask() {
