22656f6d23f01bb4fa8979128e7af74a688b1f35
iisams <69070671+iisams@users.noreply.github.com>
2021-02-10 10:03:07 +0800

    Update 7days.js

--- liwo/7days.js
+++ liwo/7days.js
@@ -49,7 +49,7 @@ const sams = new Env('梨涡签到领现金');
  }
  
  var money = ''
- var message = 'Sorry，活动于2月10日结束。新活动再见👋'
+ var message = ''
  var usermsg = ''
  var userimg =''
  
@@ -180,7 +180,7 @@ const sams = new Env('梨涡签到领现金');
    
    
    async function dotask() {
-   //await sign();
+   await sign();
    await getmoney();
    await show()
  }
